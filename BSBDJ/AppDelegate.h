552aa619026e2f956dcc80ca8d7f6e8af62ed686
孙巧进 <river520529see@163.com>
2016-01-25 18:58:20 +0800

    Initial Commit

---
+++ BSBDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ
+//
+//  Created by HopeBayBridge on 16/1/25.
+//  Copyright © 2016年 hopebaybridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSBDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSBDJ
+//
+//  Created by HopeBayBridge on 16/1/25.
+//  Copyright © 2016年 hopebaybridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
